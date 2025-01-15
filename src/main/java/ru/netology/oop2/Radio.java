f64c685d4e74ae35f4cb3a7c0b38f5ef58c91955
dmitry-mak <akkaru@gmail.com>
2025-01-15 13:15:26 +0200

    - jacoco integrated - basic functionality added

--- src/main/java/ru/netology/oop2/Radio.java
+++ src/main/java/ru/netology/oop2/Radio.java
@@ -2,28 +2,58 @@ package ru.netology.oop2;
 
 public class Radio {
 
-    public int currentStationNumber;
-    public int currentVolumeLevel;
+    private int currentStationNumber;
+    private int currentVolumeLevel;
 
-//    геттер для получения текущего номера радиостанции
+    //    геттер для получения текущего номера радиостанции
     public int getCurrentStationNumber() {
         return currentStationNumber;
     }
 
-//    геттер для получения текущего уровня громкости
+    //    геттер для получения текущего уровня громкости
     public int getCurrentVolumeLevel() {
         return currentVolumeLevel;
     }
 
-//   сеттер для установки номера радиостанции
-    public void setCurrentStationNumber(int currentStationNumber) {
-        this.currentStationNumber = currentStationNumber;
+    //   сеттер для установки номера радиостанции
+    public void setCurrentStationNumber(int newCurrentStationNumber) {
+        this.currentStationNumber = newCurrentStationNumber;
     }
 
-//    сеттер для установки уровня громкости
-    public void setCurrentVolumeLevel(int currentVolumeLevel) {
-        this.currentVolumeLevel = currentVolumeLevel;
+    //    сеттер для установки уровня громкости
+    public void setCurrentVolumeLevel(int newCurrentVolumeLevel) {
+        this.currentVolumeLevel = newCurrentVolumeLevel;
     }
 
+    public void nextStation() {
+        if (currentStationNumber == 9) {
+            setCurrentStationNumber(0);
+        } else {
+            setCurrentStationNumber(getCurrentStationNumber() + 1);
+        }
+    }
 
+    public void prevStation() {
+        if (currentStationNumber == 0) {
+            setCurrentStationNumber(9);
+        } else {
+            setCurrentStationNumber(getCurrentStationNumber() - 1);
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolumeLevel == 100) {
+            setCurrentVolumeLevel(100);
+        } else {
+            setCurrentVolumeLevel(getCurrentVolumeLevel() + 1);
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolumeLevel == 0) {
+            setCurrentVolumeLevel(0);
+        } else {
+            setCurrentVolumeLevel(getCurrentVolumeLevel() - 1);
+        }
+    }
 }
